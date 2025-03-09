ac265c226ae9861e903570e52bbd62f906791a4a
sahib-sem <semahegnsahib@gmail.com>
2025-03-09 21:06:36 +0300

    typo fix

--- auth/shema.ts
+++ auth/shema.ts
@@ -27,7 +27,7 @@ export const UserRole = z.enum(["USER", "SELLER", "ADMIN"]);
 
 export const TelegramDataSchema = z.object({
   telegramId: z.number(),
-  firstaNme: z.string(),
+  firstNme: z.string(),
   lastName: z.string().nullable(),
   username: z.string().nullable(),
   photoUrl: z.string().nullable(),
